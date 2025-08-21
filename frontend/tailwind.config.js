5ce7a6767c6d8fdee0d294a4da0f4ade99b6da7f
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-08-21 20:52:51 +0530

    organised codebase - Poushali

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,8 @@ export default {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+    "./*.{js,ts,jsx,tsx,mdx}",
+    "index.html",
   ],
   prefix: "",
   theme: {
